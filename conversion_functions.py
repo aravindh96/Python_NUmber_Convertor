90d77b1371a6800b50a0af2f200d0030c8803aaf
Aravindh Shanmuganathan <shanmuganathan.a@wustl.edu>
2020-03-26 17:17:54 -0700

    Update Comments Clean up code, edit strip_number function

--- conversion_functions.py
+++ conversion_functions.py
@@ -1,33 +1,28 @@
 
 import re
-
-phone_pad = { '0':[],
-              '1':[],
-              '2':['A','B','C'],
-              '3':['D','E','F'],
-              '4':['G','H','I'],
-              '5':['J','K','L'],
-              '6':['M','N','O'],
-              '7':['P','Q','R','S'],
-              '8':['T','U','V'],
-              '9':['W','X','Y','Z'],}
+from nltk.corpus import words
+word_set = set(words.words())
+phone_pad = {'0': [],
+             '1': [],
+             '2': ['A', 'B', 'C'],
+             '3': ['D', 'E', 'F'],
+             '4': ['G', 'H', 'I'],
+             '5': ['J', 'K', 'L'],
+             '6': ['M', 'N', 'O'],
+             '7': ['P', 'Q', 'R', 'S'],
+             '8': ['T', 'U', 'V'],
+             '9': ['W', 'X', 'Y', 'Z']}
 keypad = {}
 for key, value in phone_pad.items():
-    keypad.update(dict.fromkeys(value,key))
+    keypad.update(dict.fromkeys(value, key))
 
-# Input: Phone Number  - type:str
-# Output: Stripped Number - type:str
-# Func (strip): Removes all characters other than digits and alphabets
 """ Input: Phone Number  - type:str
     Funct (strip): Removes all characters other than digits and alphabets"""
 
 def strip_number(phone_number):
-    strip_num = re.sub('[^A-Za-z0-9]+', '', phone_number)
+    strip_num = re.sub('[^A-Za-z0-9]+', '', str(phone_number))
     return strip_num
 
-# Input: Phone Number  - type:str
-# Output: Formatted Number - type:str
-# Func (phone_format): Formats number into following pattern (d-ddd-ddd-dddd)
 """Input: Phone Number - type:str
    Funct(valid_word): Returns a valid english word in the given number"""
 def valid_word(phone_number):
