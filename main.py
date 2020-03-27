972aa0df4693a72fcc28342788e1fa10067e7d30
Aravindh Shanmuganathan <shanmuganathan.a@wustl.edu>
2020-03-26 21:53:04 -0700

    Merge new-functions into master Add valid_word() function Add testcases for all helper functions

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from conversion_functions import *
 
-print(keypad)
-print(phone_pad)
-print(strip_number("98/1-()8492"))
-print(phone_format("6546819854"))
+# print(keypad)
+# print(phone_pad)
+# print(strip_number("98/1-()8492"))
+# print(valid_word("12ringing-bells"))
+print(phone_format("12ringing-bells"))
